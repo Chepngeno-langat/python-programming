84fb011e84edcec90bbfaac3180c76c64c456a72
Chepngeno-langat <langatkarenn@gmail.com>
2022-10-11 23:31:01 +0300

    100 days of python

---
+++ Day26-list-comprehension/main.py
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3]
+square_num = [(n ** 2) for n in numbers]
+print(square_num)
+
+name = "Karen"
+new_name = [letter for letter in name]
+print(new_name)
+
+double_num = [n * 2 for n in range(1, 5)]
+print(double_num)
