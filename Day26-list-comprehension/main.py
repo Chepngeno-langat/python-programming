f7f138244d24f5c509fb3f7c108b00f389e690fa
Chepngeno-langat <langatkarenn@gmail.com>
2022-10-11 23:42:54 +0300

    Nato phonetics alphabet

--- Day26-list-comprehension/main.py
+++ Day26-list-comprehension/main.py
@@ -1,10 +1,15 @@
-numbers = [1, 2, 3]
-square_num = [(n ** 2) for n in numbers]
-print(square_num)
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-name = "Karen"
-new_name = [letter for letter in name]
-print(new_name)
+import pandas
 
-double_num = [n * 2 for n in range(1, 5)]
-print(double_num)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
