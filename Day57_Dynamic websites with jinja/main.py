7738e91677a25a67b83e4f5dcc025b114a541cec
Chepngeno-langat <langatkarenn@gmail.com>
2023-02-19 22:30:09 +0300

    urls

--- Day57_Dynamic websites with jinja/main.py
+++ Day57_Dynamic websites with jinja/main.py
@@ -26,7 +26,7 @@ def guess(name):
     return render_template("api.html", person_name=name, gender=gender, age=age)
 
 @app.route('/blog')
-def blog():
+def blog_page():
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = response.json()
