2971797f7d627132ff68cef88cdc6a00386cbda5
Chepngeno-langat <langatkarenn@gmail.com>
2023-02-21 22:34:41 +0300

    modulus

--- Day57_Dynamic websites with jinja/blog-templating/post.py
+++ Day57_Dynamic websites with jinja/blog-templating/post.py
@@ -4,3 +4,12 @@ class Post:
         self.title = title
         self.subtitle = subtitle
         self.body = body
+
+x = int(input())
+y = int(input())
+
+x = x % y
+x = x % y
+y = y % x
+
+print(y)
