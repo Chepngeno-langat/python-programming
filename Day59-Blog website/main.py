fc018bbbd66331e9d7e5bd7d42dc160212427432
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-09 14:39:26 +0300

    templating with jinja

--- Day59-Blog website/main.py
+++ Day59-Blog website/main.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+import requests
 
+posts = requests.get("https://api.npoint.io/c6b2f43138db0fa15965").json()
+
+app = Flask(__name__)
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 @app.route("/about")
 def about_page():
