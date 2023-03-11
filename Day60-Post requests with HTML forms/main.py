f38d7a152ffcf14602709fbd5546b09f7fd4c7eb
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-11 15:29:45 +0300

    post requests

--- Day60-Post requests with HTML forms/main.py
+++ Day60-Post requests with HTML forms/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h3>Name: {name}, Password: {password}</h3>"
 
 if __name__ == '__main__':
     app.run(debug=True)
