ace5f77ec3d9074e803340bfde7de24b5564110e
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-17 22:26:44 +0300

    sqlalchemy

--- Databases/main.py
+++ Databases/main.py
@@ -4,9 +4,13 @@ cur = con.cursor()
 #cur.execute("CREATE TABLE movie(title, year, score)")
 # res = cur.execute("SELECT name FROM sqlite_master")
 #cur.execute("INSERT INTO movie VALUES('Game Of Thrones', 2011, 9.0)")
-cur.execute("""
-    INSERT INTO movie VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
-""")
+# cur.execute("""
+#     INSERT INTO movie VALUES
+#         ('Monty Python and the Holy Grail', 1975, 8.2),
+#         ('And Now for Something Completely Different', 1971, 7.5)
+# """)
+
+#cur.execute("CREATE TABLE music(id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, artist varchar(250) NOT NULL)")
+# con.execute("INSERT INTO music VALUES(1, 'Would That I', 'Hozier')")
+con.execute("INSERT INTO music VALUES(2, 'Dont Stay', 'X Ambassadors')")
 con.commit()
