b8377961b5316b4386f920b82bf558f85a9cf3ba
Chepngeno-langat <langatkarenn@gmail.com>
2023-03-21 22:17:13 +0300

    new-user

--- Databases/alchemy.py
+++ Databases/alchemy.py
@@ -18,6 +18,7 @@ db.create_all()
 
 admin = User(username='admin', email='admin@example.com')
 guest = User(username='guest', email='guest@example.com')
+user = User(username='user', email='user@email.com')
 
 db.session.add(admin)
 db.session.add(guest)
